a420aa3232292fbdd91a282fcf9b6f4ae54d76db
gitrubus <93807053+gitrubus@users.noreply.github.com>
2026-03-18 14:02:18 -0400

    Implement user input for compile options
    Add option input and print statements for user feedback.

--- runner.C
+++ runner.C
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int option;
+int data = option;
 
 int main() {
     printf("welcome to the program");
     printf("use 1,2,3 for a compiling option.");
-    switch
-      case (1)
+    scanf("%d", &option);
+    printf("option is %d generating %p", option, &option);
+    if (option == 1) {
+        printf
+    }
 }
