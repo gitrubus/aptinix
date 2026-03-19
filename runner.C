fc452233d494d678a5f2414c4278641f5afd9d20
gitrubus <93807053+gitrubus@users.noreply.github.com>
2026-03-19 07:30:46 -0400

    Enhance output formatting and add new variable

--- runner.C
+++ runner.C
@@ -2,13 +2,29 @@
 
 int option;
 int data = 0;
+char hujk[30];
 
 int main() {
     printf("welcome to the program");
-    printf("use 1,2,3 for a compiling option.");
+    printf("\nuse 1,2,3 for a compiling option.");
     scanf("%d", &option);
-    printf("option is %d generating %p", option, &option);
+    printf("\noption is %d generating %p", option, &option);
     if (option == 1) {
-        printf("woring on it");
+        printf("\nworing on it");
+        while (data < 9999999) {
+            printf("\n\n%p", &data);
+            data++;
+        }
+    } else if (option == 2) {
+        printf("\naptinix");
+        scanf("\n%s", &hujk);
+        printf("\nthis is %s", hujk);
+        printf("\nconverting string into data address...");
+        printf("\n%p", &hujk);
+    } else if (option == 3) {
+        printf("\nnothing here sorry");
+    } else {
+        perror("\ninvalid option");
     }
 }
+
