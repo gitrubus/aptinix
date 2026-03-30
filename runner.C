73c35df2d7a0081000a01566ae2c2f9164d49504
gitrubus <93807053+gitrubus@users.noreply.github.com>
2026-03-30 17:10:37 -0400

    Refactor main function and update release message
    Removed old option handling and added release notice.

--- runner.C
+++ runner.C
@@ -5,26 +5,10 @@ int data = 0;
 char hujk[30];
 
 int main() {
-    printf("welcome to the program");
-    printf("\nuse 1,2,3 for a compiling option.");
-    scanf("%d", &option);
-    printf("\noption is %d generating %p", option, &option);
-    if (option == 1) {
-        printf("\nworing on it");
-        while (data < 9999999) {
-            printf("\n\n%p", &data);
-            data++;
-        }
-    } else if (option == 2) {
-        printf("\naptinix");
-        scanf("\n%s", &hujk);
-        printf("\nthis is %s", hujk);
-        printf("\nconverting string into data address...");
-        printf("\n%p", &hujk);
-    } else if (option == 3) {
-        printf("\nnothing here sorry");
-    } else {
-        perror("\ninvalid option");
-    }
+    printf("this is release of aptinix is outdated.\n");
+    printf("please use the one on gitlab\n");
+    printf("if you want to use this version use the archive branch");
+    break;
+
 }
 
